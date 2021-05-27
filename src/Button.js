210004424ec4d2fec798e086bc9118d690f471f4
Jennetiero <79895549+Jennetiero@users.noreply.github.com>
2021-05-27 17:50:37 +0500

    Add files via upload

---
+++ src/Button.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Button.css";
+import { Link } from "react-router-dom";
+
+export const Button = ({
+    children,
+    type,
+    onClick,
+    buttonStyle,
+    ButtonSize,
+}) => {
+    const checkButtonStyle = STYLES.includes{buttonStyle}
+};
